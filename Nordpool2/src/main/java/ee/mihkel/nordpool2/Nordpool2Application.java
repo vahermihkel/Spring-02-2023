7fde94bc47464ed3a41ef1eac112310ceb98bb8b
mihkelvaher <mihkel.vaher@industry62.com>
2023-02-14 18:51:08 +0200

    Uus projekt

---
+++ Nordpool2/src/main/java/ee/mihkel/nordpool2/Nordpool2Application.java
@@ -0,0 +1,13 @@
+package ee.mihkel.nordpool2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nordpool2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nordpool2Application.class, args);
+	}
+
+}
---
+++ Nordpool2/src/test/java/ee/mihkel/nordpool2/Nordpool2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.mihkel.nordpool2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nordpool2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
